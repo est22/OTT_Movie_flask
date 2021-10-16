7fff29429ebb430e5c0ad461a006d06c6e8b8ac8
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-10-16 12:20:33 +0900

    api

--- views/user.py
+++ views/user.py
@@ -19,21 +19,21 @@ def favorites():
     return like_movie_id
 
 
-@api.route('/reviews', methods=['GET', 'POST'])
-def reviews():
-    '''
-    GET : 해당 user가 작성한 모든 리뷰 데이터를 (어느영화든 상관없이) 전부 가져온다.
-    POST : (그 페이지에서) 리뷰 삭제
-    '''
-    # 사용자 작성 리뷰 테이블에서
-    # 현재 세션 username과 db의 username이 동일한 것을 골라서 모든 movie_id를 출력한다
-    if request.method == 'GET':
-        review_list = Review.query.filter(
-            Review.user_name == serssion['username']).order_by(Review.write_time.desc()).all()
-        review_movie_id = review_list.movie_id
+# @api.route('/reviews', methods=['GET', 'POST'])
+# def reviews():
+#     '''
+#     GET : 해당 user가 작성한 모든 리뷰 데이터를 (어느영화든 상관없이) 전부 가져온다.
+#     POST : (그 페이지에서) 리뷰 삭제
+#     '''
+#     # 사용자 작성 리뷰 테이블에서
+#     # 현재 세션 username과 db의 username이 동일한 것을 골라서 모든 movie_id를 출력한다
+#     if request.method == 'GET':
+#         review_list = Review.query.filter(
+#             Review.user_name == serssion['username']).order_by(Review.write_time.desc()).all()
+#         review_movie_id = review_list.movie_id
 
-        # User.query.filter((User.email == email) | (User.name == name)).first()
+#         # User.query.filter((User.email == email) | (User.name == name)).first()
 
-        return review_movie_id
+#         return review_movie_id
 
-    # # 유튜브 댓글 모아서 삭제 로직 참고
+#     # # 유튜브 댓글 모아서 삭제 로직 참고
